99211480693251baaf2db8e295b91c33e2fe6a2f
Antun Badurina <antun.badurina@infinum.hr>
2021-04-23 13:12:37 +0200

    Move example file

--- src/utils/00.example/step.ts
+++ src/utils/00.example/step.ts
@@ -1,9 +1,9 @@
 const preComputed = [1, 1];
 
-export function step(num) {
-  if (preComputed[num]) {
-    return preComputed[num];
+export function step(n) {
+  if (preComputed[n]) {
+    return preComputed[n];
   } else {
-    return step(num - 1) + step(num - 2);
+    return step(n - 1) + step(n - 2);
   }
 }
