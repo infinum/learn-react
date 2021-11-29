bbd350291ccf444eeaa467d17053fb64d940d369
Anže Pureber <anze.pureber@infinum.com>
2021-11-29 15:53:12 +0100

    add all needed exports to index

--- src/solutions/LearnReact/index.tsx
+++ src/solutions/LearnReact/index.tsx
@@ -1,4 +1,13 @@
 import { createElement } from "./createElement";
 import { render } from "./render";
+import { Component } from "./Component";
+import { useState, useEffect, FunctionalComponent } from "./hooks";
 
-export default { createElement, render };
+export default {
+  createElement,
+  render,
+  useState,
+  useEffect,
+  Component,
+  FunctionalComponent,
+};
