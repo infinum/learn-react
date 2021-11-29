5056d56692a4d9c063e0ecca5378945c0c782429
Anže Pureber <37226531+anzep@users.noreply.github.com>
2021-11-29 15:55:52 +0100

    Updated to the tests and index file (#4)
    * update tests and cleanup the index file
    
    * add all needed exports to index

--- src/solutions/LearnReact/index.tsx
+++ src/solutions/LearnReact/index.tsx
@@ -3,4 +3,11 @@ import { render } from "./render";
 import { Component } from "./Component";
 import { useState, useEffect, FunctionalComponent } from "./hooks";
 
-export default { Component };
+export default {
+  createElement,
+  render,
+  useState,
+  useEffect,
+  Component,
+  FunctionalComponent,
+};
--- src/tests/02.events/events.test.tsx
+++ src/tests/02.events/events.test.tsx
@@ -4,12 +4,18 @@ let container: HTMLElement;
 
 beforeEach(() => {
   container = document.createElement("main");
+
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  document.body.removeChild(container);
 });
 
 test("When the button is clicked, the function assigned to an onClick event is triggered", () => {
   // YOUR TEST GOES HERE
 });
 
-test("When the button is clicked, the function assigned to an onBlur event is triggered", () => {
+test("When focus from the button is removed, the function assigned to an onBlur event is triggered", () => {
   // YOUR TEST GOES HERE
 });
